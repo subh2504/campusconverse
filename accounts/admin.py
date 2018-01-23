059495f3d3f66dd1ff041b98d76d89c3bbffbf00
Subhash Hardaha <subhashhardaha@gmail.com>
2018-01-24 00:46:43 +0530

    image in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from campus.models import *
+
+
+class ImageAdmin(admin.ModelAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    fields = ('campus_owner','campus_name','campus_address','campus_locality','campus_city','campus_country','campus_pincode','campus_logo','image_tag')
+    readonly_fields = ('image_tag',)
+
+
+admin.site.register(CampusDetail, ImageAdmin)
+admin.site.register(CampusCourseDepart)
+admin.site.register(MasterCourse)
+admin.site.register(MasterDepart)
--- campus/models.py
+++ campus/models.py
@@ -1,22 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+from campusconverse import settings
+
 
 class CampusDetail(models.Model):
-    campus_id = models.IntegerField(primary_key=True)
-    campus_owner = models.For
+    campus_id = models.AutoField(primary_key=True)
+    campus_owner = models.ForeignKey(User, on_delete=models.CASCADE)
     campus_name = models.CharField(max_length=100)
-    campus_address = models.CharField(max_length=100,blank=True,null=True)
-    campus_locality = models.CharField(max_length=100,blank=True,null=True)
-    campus_city = models.CharField(max_length=20,blank=True,null=True)
-    campus_country = models.CharField(max_length=20,blank=True,null=True)
-    campus_pincode = models.CharField(max_length=6,blank=True,null=True)
-    campus_logo = models.CharField(max_length=15)
+    campus_address = models.CharField(max_length=100, blank=True, null=True)
+    campus_locality = models.CharField(max_length=100, blank=True, null=True)
+    campus_city = models.CharField(max_length=20, blank=True, null=True)
+    campus_country = models.CharField(max_length=20, blank=True, null=True)
+    campus_pincode = models.CharField(max_length=6, blank=True, null=True)
+    campus_logo = models.ImageField(upload_to='product', blank=True)
+
+    def image_tag(self):
+        return mark_safe('<img src="/media/%s" />' % (self.campus_logo))
+    image_tag.short_description = 'Image'
+    image_tag.allow_tags = True
 
     def __str__(self):
         return self.campus_name
-        
-        
+
+
 class MasterDepart(models.Model):
     depart_id = models.IntegerField(primary_key=True)
     depart_name = models.CharField(unique=True, max_length=100)
@@ -32,20 +43,21 @@ class MasterCourse(models.Model):
     def __str__(self):
         return self.course_name
 
-class Desination(models.Model):
+
+class Designation(models.Model):
     designation_id = models.IntegerField(primary_key=True)
     designation_name = models.CharField(unique=True, max_length=50)
     type = models.IntegerField()
 
     def __str__(self):
         return self.designation_name
-        
-        
+
+
 class CampusCourseDepart(models.Model):
-    ccd_id= models.IntegerField(primary_key=True)
-    campus = models.ForeignKey('CollegeDetail')
-    course = models.ForeignKey('MasterCourse')
-    department = models.ForeignKey('MasterDepart')
+    ccd_id = models.IntegerField(primary_key=True)
+    campus = models.ForeignKey(CampusDetail, on_delete=models.CASCADE)
+    course = models.ForeignKey(MasterCourse, on_delete=models.CASCADE)
+    department = models.ForeignKey(MasterDepart, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.college.college_name + " " + self.course.course_name + " " + self.depart.depart_name
